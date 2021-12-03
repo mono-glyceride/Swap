704c2886797feb8a5e46f79440908b5d34398206
gurido <glyceride.mono@gmail.com>
2021-12-03 05:20:22 +0000

    Vue_Bootstrap_npm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <title>Swap</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
         <link rel="stylesheet" href="css/footer.css">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="/css/main.css">
@@ -34,9 +35,6 @@
         @include('commons.footer')
         @endauth
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+       
     </body>
 </html>
