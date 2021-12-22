7f5b6546461079b9d44c7c4f30f6a03bf7998d48
gurido <glyceride.mono@gmail.com>
2021-12-22 00:51:12 +0000

    sm_size

--- resources/views/exhibits/show.blade.php
+++ resources/views/exhibits/show.blade.php
@@ -23,7 +23,7 @@
                     <table class="table table-sm item_table">
                         <tr><th>キャラクター（譲）</th><td>{{ $exhibit->character }}</td></tr>
                         <tr><th>キャラクター（求）</th><td>{{ $exhibit->want_character }}</td></tr>
-                        <tr><th>交換方法</th></th><td>{{ $mail_flag }}/{{ $handing_flag }}</td></tr>
+                        <tr><th>交換方法</th></th><td>郵送に{{ $mail_flag }}/手渡しに{{ $handing_flag }}</td></tr>
                         <tr><th>手渡し対応地域</th><td>{{ $exhibit->place }}</td></tr>
                         <tr><th>補足説明</th><td>{{ $exhibit->notes }}</td></tr>
                     </table>
