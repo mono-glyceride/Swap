8cd97ac9f1943162cd16def88d96cf6515ad6ccf
gurido <glyceride.mono@gmail.com>
2022-01-19 07:09:03 +0000

    notifications_front

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -2,22 +2,27 @@
 
 
 @section('content')
-        <div class="text-center border border-primary midasi">
-                <h1>
-                    通知
-                </h1>
-        </div>
-        <ul>
-        @if (count($notifications) > 0)      
-                @foreach ($notifications as $notification)
-                        @if($notification->content_id == 0)
-                            {{ Form::open(['route' => ['notifications.update',$notification->id],'method' => 'patch','class'=>'form-horizontal']) }}
-                                {{Form::hidden('status','1')}}
-                                <button type="submit" class="btn btn-light"><img src="{{$notification->exhibit->pic_id }}" width="70" height="70" >
-                                <div>「{{$notification->exhibit->character}} {{$notification->exhibit->goods_type}}」への交換リクエストは成立しませんでした</div></button>
-                            {{ Form::close() }}
-                        @endif
-                @endforeach
+    <header class="sticky-top d-block d-sm-none">
+        <h1 class="d-flex align-items-center">
+            @include('commons.back_button')
+            通知
+        </h1>
+    </header>
+    @if (count($notifications) > 0)      
+        @foreach ($notifications as $notification)
+            @if($notification->content_id == 0)
+                {{ Form::open(['route' => ['notifications.update',$notification->id],'method' => 'patch','class'=>'form-horizontal']) }}
+                {{Form::hidden('status','1')}}    
+                <div class="lists">    
+                    <div class="row align-items-center d-flex">    
+                        <img src="{{$notification->exhibit->pic_id }}" class="col-3 listsimg">
+                        <div class="col-9">
+                            「{{$notification->exhibit->character}} {{$notification->exhibit->goods_type}}」への交換リクエストは成立しませんでした
+                        </div>
+                    </div>
+                </div>
+                {{ Form::close() }}
             @endif
-        </ul>
+        @endforeach
+    @endif
 @endsection
