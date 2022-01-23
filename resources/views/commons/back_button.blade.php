7ec90bf079f9883104f2bcec5519f48e370b2cac
gurido <glyceride.mono@gmail.com>
2022-01-23 11:43:47 +0000

    delete_back_button

--- resources/views/commons/back_button.blade.php
+++ resources/views/commons/back_button.blade.php
@@ -1,2 +1,2 @@
-<button type="button" class="btn back_button rounded-circle col-2" onClick="history.back()"><i class="fas fa-chevron-left"></i></button>
+{{-- <button type="button" class="btn back_button rounded-circle col-2" onClick="history.back()"><i class="fas fa-chevron-left"></i></button>--}}
 <!-- <i class="fas fa-angle-left back_button" >-->
